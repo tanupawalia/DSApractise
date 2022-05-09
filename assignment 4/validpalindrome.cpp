9835586656b5a2b3fc61424fd870e62cf8aa87f3
tanu pawalia <tanupawalia@gmail.com>
2022-05-09 22:08:34 +0530

    Q5

--- assignment 4/validpalindrome.cpp
+++ assignment 4/validpalindrome.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if(s.length() == 0)
-            return true;
-        int i = 0, j = s.length() - 1;
-        while(i < j) {
-            while(i < j && !isalnum(s[i])) {
-                i++;
-            }
-            while(i < j && !isalnum(s[j])) {
-                j--;
-            }
-            if(i < j && tolower(s[i]) != tolower(s[j])) {
-                return false;
-            } else {
-                i++;
-                j--;
-            }
+        
+    string s2=""; 
+        
+        
+        int len=s.length(); 
+        
+        for(int i=0;i<len;i++){
+            
+            if(isalpha(s[i])||isdigit(s[i])){
+            s[i]=tolower(s[i]);
+            s2+=s[i];
+            
         }
-        return true;
+        }
+        
+        s=s2;
+        reverse(s2.begin(),s2.end());
+        
+        return s2==s;
+                            
+        
+        
+    
     }
 };
