5b955622935d12b4acea0c3f8cb7e72d87c3d1da
tanu pawalia <tanupawalia@gmail.com>
2022-07-01 15:29:38 +0530

    permutation2

--- assignment 16/permutation2.cpp
+++ assignment 16/permutation2.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void recursion(int i,vector<int> nums, vector<vector<int>> &ans){
-        if(i>=nums.size()){
-            ans.push_back(nums);
+    void recursion(int i,vector<int> A, vector<vector<int>> &ans){
+        if(i>=A.size()){
+            ans.push_back(A);
             return;
         }
-        for(int j=i;j<nums.size();j++){
-           swap(nums[i],nums[j]);
-            recursion(i+1,nums,ans);
-            swap(nums[i],nums[j]);
+        for(int j=i;j<A.size();j++){
+           swap(A[i],A[j]);
+            recursion(i+1,A,ans);
+            swap(A[i],A[j]);
         }
     }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
+    vector<vector<int>> permuteUnique(vector<int>& A) {
         vector<vector<int>> ans;
-        int index=0;
-        recursion(index,nums,ans);
+
+        recursion(0,A,ans);
         sort(ans.begin(),ans.end());
         ans.erase(unique(ans.begin(),ans.end()),ans.end());
         return ans;
