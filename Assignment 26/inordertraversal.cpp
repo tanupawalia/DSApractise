250c50d184fbf70cc9d9b39179a5ff3bf8d21b09
tanu pawalia <tanupawalia@gmail.com>
2022-07-14 09:50:24 +0530

    inorder

---
+++ Assignment 26/inordertraversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> inorderTraversal(TreeNode* root) {
+
+
+    if (root == NULL)
+        return ans;
+        inorderTraversal(root->left);
+        ans.push_back(root->val);
+        cout<<root->val;
+
+        inorderTraversal(root->right);
+        
+        return ans;
+}
+   
+};
