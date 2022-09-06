2f3f82e0755c9a68f7cf9ec747ed56f0e315f675
tanupawalia <60600404+tanupawalia@users.noreply.github.com>
2022-09-06 22:26:04 +0530

    Create minsteps.cpp

---
+++ Assignment24/minsteps.cpp
@@ -0,0 +1,10 @@
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+    
+  int n=A.size();
+  int steps=0;
+  for(int i=0;i<n-1;i++){
+      steps +=max(abs(A[i+1]-A[i]) , abs(B[i+1]-B[i]) );
+  }  
+  return steps;
+    
+}
