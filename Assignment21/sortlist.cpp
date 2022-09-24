ce05d8099af99100fa16178ddc5627d10e091041
tanupawalia <60600404+tanupawalia@users.noreply.github.com>
2022-09-24 19:55:24 +0530

    Create sortlist.cpp

---
+++ Assignment21/sortlist.cpp
@@ -0,0 +1,50 @@
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* i = list1;
+        ListNode* j = list2;
+        ListNode* temp = new ListNode(-1);
+        ListNode* k = temp;
+        
+        while(i!=nullptr && j!=nullptr){
+            if(i->val >= j->val){
+                k->next = j;
+                j = j->next;
+            }
+            else{
+                k->next = i;
+                i = i->next;
+            }
+               k = k->next;
+        }
+        if(i!=nullptr){
+            k->next = i;
+        }
+        if(j!=nullptr){
+            k->next = j;
+        }
+        return temp->next;
+    }
+    
+ListNode* Solution::sortList(ListNode* A) {
+    if(!A || !A->next) return A;
+    ListNode* head = A;
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+
+    while(fast){
+        fast = fast->next;
+        if(fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+    ListNode* left = head;
+    ListNode* right = slow->next;
+    slow->next = NULL;
+     left = sortList(left);
+     right = sortList(right);
+
+     if(!left) return right;
+     if(!right) return left;
+
+     return mergeTwoLists(left,right);
+}
