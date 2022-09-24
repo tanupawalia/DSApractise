6195059f5491bac6f95f32977b370985e4f1a818
tanupawalia <60600404+tanupawalia@users.noreply.github.com>
2022-09-24 19:45:57 +0530

    Create Q3.cpp

---
+++ Assignment13/Q3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+             ListNode* cur = head;
+        ListNode* prev = head;
+        while(cur!=NULL)
+        {
+            ListNode* next = cur->next;
+            if(cur==head)
+            {
+                cur->next = NULL;
+            }else
+            {
+                cur->next = prev;
+            }
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+};
